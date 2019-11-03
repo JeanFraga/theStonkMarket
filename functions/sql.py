a1fc06789c8420f77f6c7e11ea7c870f93d093aa
distortedlogic <jermeek@users.noreply.github.com>
2019-11-03 18:01:29 -0500

    added new functions

--- functions/sql.py
+++ functions/sql.py
@@ -1,5 +1,35 @@
 import sqlite3
+import time
+from datetime import datetime
 
+def set_time_range(current_table, year, month):
+    dt = datetime(year=year, month=month, day=1, hour=0, minute=0)
+    fresh_month_ts = time.mktime(dt.timetuple())
+
+    max_db_time = get_max_timestamp(current_table)
+    if not max_db_time:
+        max_db_time = fresh_month_ts
+
+    if month == 12:
+        next_month = 1
+        year += 1
+    else:
+        next_month = month+1
+
+    dt = datetime(year=year, month=next_month, day=1, hour=0, minute=0)
+    next_month_ts = time.mktime(dt.timetuple())
+
+    return max_db_time, next_month_ts
+
+def table_prep(subreddit, year, month):
+    current_table = f'{subreddit}_{year}_{month}'
+
+    with sqlite3.connect("memes.db") as db:
+        exists = db.cursor().execute(check_table_exists(current_table)).fetchall()
+    if not exists:
+        create_meme_table(current_table)
+    
+    return current_table
 
 def get_max_timestamp(current_table):
     max_ts_str = f'''SELECT MAX(timestamp) FROM {current_table}'''
@@ -12,6 +42,8 @@ def insert_meme_sql_string(current_table, names_list):
     col_ord_str = str(names_list)[1:-1].replace("'", "")
     return f''' INSERT INTO {current_table}({col_ord_str}) VALUES({','.join(['?']*len(names_list))}) '''
 
+def check_table_exists(current_table):
+    return f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{current_table}';"""
 
 def create_meme_table(name):
     cols = """(
--- functions/workerdb_compilers.py
+++ functions/workerdb_compilers.py
@@ -1,5 +1,4 @@
 import os
-import requests
 import json
 import pandas as pd
 import sqlite3
@@ -9,6 +8,10 @@ from functions.sql import insert_meme_sql_string
 
 worker_db_uri = r'worker_dbs/db_{}.json'
 
+def clear_worker_dbs():
+    for i in range(0, 8):
+        try: os.remove(worker_db_uri.format(i))
+        except: pass
 
 def insert_new_data(current_table):
     cdf = pd.DataFrame()
