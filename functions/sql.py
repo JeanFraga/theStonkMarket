ec323e19968a210ba461bac0dab2db930787d978
distortedlogic <jermeek@users.noreply.github.com>
2019-11-04 20:43:22 -0500

    upgrade

--- functions/sql.py
+++ functions/sql.py
@@ -1,6 +1,10 @@
 import sqlite3
 import time
 from datetime import datetime
+import time
+import pandas as pd
+import os
+from functions.constants import MONTH_TD, LAG_15MIN
 
 def set_time_range(current_table, year, month):
     dt = datetime(year=year, month=month, day=1, hour=0, minute=0)
@@ -21,53 +25,91 @@ def set_time_range(current_table, year, month):
 
     return max_db_time, next_month_ts
 
-def table_prep(subreddit, year, month):
-    current_table = f'{subreddit}_{year}_{month}'
-
-    with sqlite3.connect("memes.db") as db:
+def table_prep(current_table, cols=''):
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
         exists = db.cursor().execute(check_table_exists(current_table)).fetchall()
     if not exists:
-        create_meme_table(current_table)
-    
-    return current_table
+        create_table(current_table, cols=cols)
+        return True
+    return False
 
 def get_max_timestamp(current_table):
     max_ts_str = f'''SELECT MAX(timestamp) FROM {current_table}'''
-    with sqlite3.connect("memes.db") as db:
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
         max_db_time = db.cursor().execute(max_ts_str)
         return max_db_time.fetchall()[0][0]
 
+def get_latest_month_memedata(subreddit):
+    global MONTH_TD
+    global LAG_15MIN
+    
+    now= int(time.time() - LAG_15MIN)
+    data_table = f'{subreddit}_scoring'
+    select_latest_month = f"""
+        Select *
+        FROM {data_table}
+        WHERE timestamp > {now-MONTH_TD}
+    """
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
+        return pd.read_sql(select_latest_month, db)
 
-def insert_meme_sql_string(current_table, names_list):
-    col_ord_str = str(names_list)[1:-1].replace("'", "")
-    return f''' INSERT INTO {current_table}({col_ord_str}) VALUES({','.join(['?']*len(names_list))}) '''
+def insert_meme_data(current_table, cols):
+    col_ord_str = str(cols)[1:-1].replace("'", "")
+    return f''' INSERT INTO {current_table}({col_ord_str}) VALUES({','.join(['?']*len(cols))}) '''
 
 def check_table_exists(current_table):
     return f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{current_table}';"""
 
-def create_meme_table(name):
-    cols = """(
-        id TEXT,
-        title TEXT,
-        author TEXT,
-        media TEXT,
-        meme_text TEXT,
-        status TEXT,
-        timestamp INTEGER,
-        datetime DATETIME,
-        year INTEGER,
-        month INTEGER,
-        day INTEGER,
-        hour INTEGER,
-        minute INTEGER,
-        upvote_ratio FLOAT,
-        upvotes INTEGER,
-        downvotes INTEGER,
-        nsfw BOOL,
-        num_comments INTEGER
-    );"""
+def get_db_list():
+    db_list = []
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
+        for db_name in db.cursor().execute("SELECT name FROM sqlite_master WHERE type = 'table'"):
+            db_list.append(db_name[0])
+    return db_list
 
-    sql_create_table = f'CREATE TABLE IF NOT EXISTS {name}' + cols
+def remove_duplicates(current_table):
+    delete_dups_str = f"""
+        DELETE FROM {current_table}
+        WHERE rowid NOT IN (
+            SELECT min(rowid)
+            FROM {current_table}
+            GROUP BY id
+        ); 
+    """
 
-    with sqlite3.connect("memes.db") as db:
+    print(delete_dups_str)
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
+        db.cursor().execute(delete_dups_str)
+
+def remove_dups_all_tables():
+    db_list = get_db_list()
+    for db_name in db_list:
+        remove_duplicates(db_name)
+
+def create_table(name, cols=''):
+    if not cols:
+        cols = """
+            id TEXT UNIQUE,
+            title TEXT,
+            author TEXT,
+            media TEXT,
+            meme_text TEXT,
+            status TEXT,
+            timestamp INTEGER,
+            datetime DATETIME,
+            year INTEGER,
+            month INTEGER,
+            day INTEGER,
+            hour INTEGER,
+            minute INTEGER,
+            upvote_ratio FLOAT,
+            upvotes INTEGER,
+            downvotes INTEGER,
+            nsfw BOOL,
+            num_comments INTEGER
+        """
+
+    sql_create_table = f'CREATE TABLE IF NOT EXISTS {name}(' + cols + ');'
+
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
         db.cursor().execute(sql_create_table)
--- functions/workerdb_compilers.py
+++ functions/workerdb_compilers.py
@@ -3,37 +3,35 @@ import json
 import pandas as pd
 import sqlite3
 
-from functions.sql import insert_meme_sql_string
-
-
-worker_db_uri = r'worker_dbs/db_{}.json'
+from functions.sql import insert_meme_data
+from functions.constants import WORKER_DB_URI
 
 def clear_worker_dbs():
     for i in range(0, 8):
-        try: os.remove(worker_db_uri.format(i))
+        try: os.remove(WORKER_DB_URI.format(i))
         except: pass
 
 def insert_new_data(current_table):
     cdf = pd.DataFrame()
     for i in range(0, 8):
-        with open(worker_db_uri.format(i), "r") as data:
+        with open(WORKER_DB_URI.format(i), "r") as data:
             json_data = json.load(data)
 
         df = pd.DataFrame.from_dict(json_data['_default']).transpose()
         cdf = pd.concat([cdf, df], ignore_index=True, axis=0, sort=True)
-        os.remove(worker_db_uri.format(i))
+        os.remove(WORKER_DB_URI.format(i))
 
-    with sqlite3.connect("memes.db") as db:
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
         data = list(cdf.itertuples(index=False, name=None))
-        db.cursor().executemany(insert_meme_sql_string(current_table, list(cdf)), data)
+        db.cursor().executemany(insert_meme_data(current_table, list(cdf)), data)
 
 
 def update_status_col(current_table):
     cdf = pd.DataFrame(columns=['id', 'status'])
     for i in range(0, 8):
-        with open(worker_db_uri.format(i), "r") as data:
+        with open(WORKER_DB_URI.format(i), "r") as data:
             json_data = json.load(data)
-        os.remove(worker_db_uri.format(i))
+        os.remove(WORKER_DB_URI.format(i))
 
         df = pd.DataFrame.from_dict(json_data['_default']).transpose()
         cdf = pd.concat([cdf, df], ignore_index=True, axis=0, sort=False)
@@ -42,6 +40,6 @@ def update_status_col(current_table):
                 SET status = ?
                 WHERE id = ?; '''
 
-    with sqlite3.connect("memes.db") as db:
+    with sqlite3.connect(os.environ['DB_PATH']) as db:
         data = list(cdf[['status', 'id']].itertuples(index=False, name=None))
         db.cursor().executemany(sql_str, data)
