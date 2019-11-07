584224226309b9e081194173a8f307260dcf4b11
distortedlogic <jermeek@users.noreply.github.com>
2019-11-07 17:35:07 -0500

    major upgrade

--- functions/sql.py
+++ functions/sql.py
@@ -4,7 +4,10 @@ from datetime import datetime
 import time
 import pandas as pd
 import os
-from functions.constants import MONTH_TD, LAG_15MIN, DB_PATH
+from time import time, mktime
+from functions.constants import MONTH_TD, DB_PATH
+
+# SQL strings #####################################################################
 
 memedata_schema = """
     id TEXT,
@@ -27,49 +30,20 @@ memedata_schema = """
     num_comments INTEGER
 """
 
-def set_time_range(table, year, month, day=1, hour=0, minute=0):
-    dt = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
-    fresh_month_ts = time.mktime(dt.timetuple())
-
-    max_db_time = get_max_timestamp(table)
-    if not max_db_time:
-        max_db_time = fresh_month_ts
-
-    if month == 12:
-        next_month = 1
-        year += 1
-    else:
-        next_month = month+1
-
-    dt = datetime(year=year, month=next_month, day=day, hour=hour, minute=minute)
-    next_month_ts = time.mktime(dt.timetuple())
-
-    return max_db_time, next_month_ts
-
-def table_prep(table, cols=''):
-    with sqlite3.connect(DB_PATH) as db:
-        exists = db.cursor().execute(check_table_exists(table)).fetchall()
-    if not exists:
-        create_table(table, cols=cols)
-        return True
-    return False
-
-def get_max_timestamp(table):
-    max_ts_str = f'''SELECT MAX(timestamp) FROM {table}'''
-    with sqlite3.connect(DB_PATH) as db:
-        max_db_time = db.cursor().execute(max_ts_str)
-        return max_db_time.fetchall()[0][0]
-
-def get_scoring_df(subreddit):
-    now= int(time.time() - LAG_15MIN)
-    data_table = f'{subreddit}_scoring'
-    select_latest_month = f"""
-        Select *
-        FROM {data_table}
-        WHERE timestamp > {now-MONTH_TD}
+select_from = """
+        Select {}
+        FROM {}
     """
+
+# data loaders #####################################################################
+
+def get_scoring_df(subreddit, cols):
+    col_ord_str = str(cols)[1:-1].replace("'", "")
+    table = f'{subreddit}_scoring'
     with sqlite3.connect(DB_PATH) as db:
-        return pd.read_sql(select_latest_month, db)
+        return pd.read_sql(select_from.format(col_ord_str, table), db)
+
+# generate sql string ##################################################################### 
 
 def insert_meme_data(table, cols):
     col_ord_str = str(cols)[1:-1].replace("'", "")
@@ -82,10 +56,18 @@ def get_meme_data(table, cols, condition):
                     WHERE {condition}
                 '''
     col_ord_str = str(cols)[1:-1].replace("'", "")
-    return f''' INSERT INTO {table}({col_ord_str}) VALUES({','.join(['?']*len(cols))}) '''
+    return f''' SELECT {col_ord_str} FROM {table} WHERE {condition}'''
 
 def check_table_exists(table):
     return f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';"""
+
+# get info #####################################################################
+
+def get_max_timestamp(table):
+    max_ts_str = f'''SELECT MAX(timestamp) FROM {table}'''
+    with sqlite3.connect(DB_PATH) as db:
+        max_db_time = db.cursor().execute(max_ts_str)
+    return max_db_time.fetchall()[0][0]
 
 def get_table_list():
     db_list = []
@@ -93,6 +75,36 @@ def get_table_list():
         for db_name in db.cursor().execute("SELECT name FROM sqlite_master WHERE type = 'table'"):
             db_list.append(db_name[0])
     return db_list
+
+def get_time_range(table, year, month, day=1, hour=0, minute=0):
+    dt = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+    fresh_month_ts = mktime(dt.timetuple())
+
+    max_db_time = get_max_timestamp(table)
+    if not max_db_time:
+        max_db_time = fresh_month_ts
+
+    if month == 12:
+        next_month = 1
+        year += 1
+    else:
+        next_month = month+1
+
+    dt = datetime(year=year, month=next_month, day=day, hour=hour, minute=minute)
+    next_month_ts = mktime(dt.timetuple())
+
+    return max_db_time, next_month_ts
+
+# removers #####################################################################
+
+def del_over_month_old(table):
+    month_ago = int(time()) - MONTH_TD
+    delete_over_month_old_str = f"""
+        DELETE FROM {table}
+        WHERE timestamp < {month_ago}; 
+    """
+    with sqlite3.connect(DB_PATH) as db:
+        db.cursor().execute(delete_over_month_old_str)
 
 def remove_duplicates(table):
     delete_dups_str = f"""
@@ -112,6 +124,8 @@ def remove_dups_all_tables():
     for db_name in db_list:
         remove_duplicates(db_name)
 
+# creators #####################################################################
+
 def create_table(name, cols=''):
     if not cols:
         global memedata_schema
@@ -121,3 +135,11 @@ def create_table(name, cols=''):
 
     with sqlite3.connect(DB_PATH) as db:
         db.cursor().execute(sql_create_table)
+
+def table_prep(table, cols=''):
+    with sqlite3.connect(DB_PATH) as db:
+        exists = db.cursor().execute(check_table_exists(table)).fetchall()
+    if not exists:
+        create_table(table, cols=cols)
+        return True
+    return False
