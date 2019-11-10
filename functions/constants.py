4232a6c9965ec28d4b18218038b30f4e47f48471
distortedlogic <jermeek@users.noreply.github.com>
2019-11-10 13:08:14 -0500

    housekeeping

--- functions/constants.py
+++ functions/constants.py
@@ -3,7 +3,7 @@ import os
 DEBUG_REDDIT = True
 MONTH_TD = 60*60*24*30
 DAY_TD = 60*60*24
-WORKER_DB_URI = r'worker_dbs/db_{}.json'
+WORKER_DB_URI = r'databases/worker_dbs/db_{}.json'
 FILE_TYPES = [".jpg", ".jpeg", ".png"]
 NUM_WORKERS = int(os.environ['NUM_WORKERS'])
 DB_PATH = os.environ['DB_PATH']
