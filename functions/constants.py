8ebb95f116292c4a973bc12371cf09012ce733d7
distortedlogic <jermeek@users.noreply.github.com>
2019-11-11 09:34:32 -0500

    code reorg

--- functions/constants.py
+++ functions/constants.py
@@ -4,8 +4,10 @@ from multiprocessing import cpu_count
 DEBUG_REDDIT = True
 MONTH_TD = 60*60*24*30
 DAY_TD = 60*60*24
-WORKER_DB_URI = environ['WORKER_DB_URI']
 FILE_TYPES = [".jpg", ".jpeg", ".png"]
+
 NUM_WORKERS = cpu_count()
+
+WORKER_DB_URI = environ['WORKER_DB_URI']
 DB_PATH = environ['DB_PATH']
-IMGDIR_PATH = 'databases/imgdir'
+IMGDIR_PATH = environ['databases/imgdir']
