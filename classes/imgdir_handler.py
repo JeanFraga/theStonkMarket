b2b6f7998a99aba1a32e28931938c34df39c53e2
distortedlogic <jermeek@users.noreply.github.com>
2019-11-11 09:32:04 -0500

    bring up to speed with latest updates

--- classes/imgdir_handler.py
+++ classes/imgdir_handler.py
@@ -10,55 +10,43 @@ import json
 import requests
 
 from PIL import Image
-from PIL import ImageChops
-from cv2 import cv2
+from PIL.ImageOps import fit
 
 import tqdm
 import praw
 import multiprocessing
+from multiprocessing import current_process
 
 from keras.preprocessing.image import ImageDataGenerator
 
+from functions.misc import check_isDeleted
 from functions.workerdb_compilers import update_status_col
-
-
-def check_isDeleted(output_filename):
-    deleted = cv2.imread('assets/image404.png')
-    deleted_nb = cv2.imread('assets/image404_nb.png')
-    image = cv2.imread(output_filename)
-
-    try:
-        diff = cv2.subtract(image, deleted)
-    except:
-        diff = True
-    try:
-        diff_nb = cv2.subtract(image, deleted_nb)
-    except:
-        diff_nb = True
-
-    return (np.all(diff == 0) | np.all(diff_nb == 0))
-
+from functions.constants import (
+    IMGDIR_PATH,
+    WORKER_DB_URI,
+    NUM_WORKERS,
+    DB_PATH
+)
 
 def download(lst):
-    imgdir_path = 'imgdir'
     post_id, post_url = lst
-
-    output_filename = os.path.join(
-        imgdir_path, f"{post_id}.png")
-    _data = {
-        'id': post_id
-    }
+    output_filename = os.path.join(IMGDIR_PATH, f"{post_id}.png")
+    _data = {'id': post_id}
 
     try:
         with requests.get(post_url) as response, open(output_filename, 'wb') as out_file:
-            i = Image.open(io.BytesIO(response.content))
-            i.save(out_file, format='png')
+            img = Image.open(io.BytesIO(response.content))
+            img.save(out_file, format='png')
 
         if check_isDeleted(output_filename):
             os.remove(output_filename)
             _data['status'] = 'deleted'
             worker_db.insert(_data)
         else:
+            with open(output_filename, 'r+') as out_file:
+                raw_img = Image.open(output_filename)
+                img = fit(raw_img, (224, 224), Image.ANTIALIAS)
+                img.save(output_filename, format='png')
             _data['status'] = output_filename
             worker_db.insert(_data)
     except:
@@ -67,28 +55,20 @@ def download(lst):
 
 
 def initializer():
-
     global worker_db
-    WORKER_DB_URI = 'worker_dbs/db_{}.json'
-
-    worker_id = (
-        int(multiprocessing.current_process().name.split("-", 1)[1])-1) % 8
-
+    worker_id = (int(current_process().name.split("-", 1)[1])-1) % NUM_WORKERS
     worker_db = TinyDB(WORKER_DB_URI.format(worker_id))
 
 
-class imgdir_handler:
+class Imgdir_Handler:
     def __init__(self, chunksize=100):
-        self.NUM_WORKERS = 8
-
-        self.db_path = os.environ['DB_PATH']
-        self.imgdir_path = 'imgdir'
 
         self.id_column = 'id'
         self.imgurl_column = 'media'
 
         self.chunksize = chunksize
         self.current_ids = None
+        self.tables = ['dankmemes_scoring']
 
         self.chunk_counter = -1
 
@@ -102,7 +82,7 @@ class imgdir_handler:
 
     def imgdata_generator(self):
         return ImageDataGenerator(rescale=1./255).flow_from_directory(
-            self.imgdir_path,
+            IMGDIR_PATH,
             target_size=(150, 150),
             batch_size=32,
             class_mode='binary'
@@ -110,8 +90,12 @@ class imgdir_handler:
 
     def imgdir_generator(self):
         for table in self.tables:
-            sql_str = f'''SELECT {self.id_column}, {self.imgurl_column} FROM {table} WHERE status IS NULL'''
-            with sqlite3.connect(self.db_path) as db:
+            sql_str = f'''
+                SELECT {self.id_column}, {self.imgurl_column}
+                FROM {table}
+                WHERE status IS NULL;
+            '''
+            with sqlite3.connect(DB_PATH) as db:
                 df = pd.read_sql(sql_str, db)
             if df.empty:
                 print("This table's status column is filled")
@@ -122,12 +106,12 @@ class imgdir_handler:
 
                 for chunk in np.array_split(data, num_chunks):
 
-                    filelist = [f for f in os.listdir(self.imgdir_path)]
+                    filelist = [f for f in os.listdir(IMGDIR_PATH)]
                     if filelist:
                         for f in filelist:
-                            os.remove(os.path.join(self.imgdir_path, f))
+                            os.remove(os.path.join(IMGDIR_PATH, f))
 
-                    p = multiprocessing.Pool(self.NUM_WORKERS, initializer)
+                    p = multiprocessing.Pool(NUM_WORKERS, initializer)
                     list(tqdm.tqdm(p.imap_unordered(
                         download, chunk), total=len(chunk)))
                     p.close()
