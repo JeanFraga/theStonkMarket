2040114bf9153dd16b711d00cf52f4c3681396c5
distortedlogic <jermeek@users.noreply.github.com>
2019-11-11 09:31:42 -0500

    moved in new func

--- functions/misc.py
+++ functions/misc.py
@@ -2,6 +2,8 @@ import logging
 from praw import Reddit
 from json import loads
 from os import environ
+from cv2 import cv2
+import numpy as np
 
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(process)d:%(message)s')
 
@@ -25,3 +27,19 @@ def init_reddit(id):
                     password=reddit_oauth['PASSWORD'],
                     user_agent=reddit_oauth['USERAGENT'],
                     username=reddit_oauth['USERNAME'])
+
+def check_isDeleted(output_filename):
+    deleted = cv2.imread('assets/deleted_img/image404.png')
+    deleted_nb = cv2.imread('assets/deleted_img/image404_nb.png')
+    image = cv2.imread(output_filename)
+
+    try:
+        diff = cv2.subtract(image, deleted)
+    except:
+        diff = True
+    try:
+        diff_nb = cv2.subtract(image, deleted_nb)
+    except:
+        diff_nb = True
+
+    return (np.all(diff == 0) | np.all(diff_nb == 0))
