8fd9d09debe941d661d3cdea147384e13c46b23c
distortedlogic <jermeek@users.noreply.github.com>
2019-11-27 16:53:07 -0500

    add waitress

--- Stonks/__init__.py
+++ Stonks/__init__.py
@@ -1,7 +1,8 @@
 from .app import create_app
+from waitress import serve
 
 #may require tinkering depending on how gunicron is set up
 
 APP = create_app()
 if __name__ == "__main__":
-    APP.run(host="0.0.0.0", port=5000,debug=False)
+    serve(APP, host="0.0.0.0", port=5000)
