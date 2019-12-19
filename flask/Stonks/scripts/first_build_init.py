bd7a1f7134238ade67933e9d4d4fb2d5bad6d192
distortedlogic <jermeek@users.noreply.github.com>
2019-12-19 16:06:28 -0500

    Update first_build_init.py

--- flask/Stonks/scripts/first_build_init.py
+++ flask/Stonks/scripts/first_build_init.py
@@ -1,14 +1,17 @@
 from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+from flask import current_app
+
 from templates import build_template_db
 from schema import DB
 
 
 def build_db():
-    DB.drop_all()
-    DB.create_all()
-    build_template_db()
+    with current_app.app_context():
+        DB.drop_all()
+        DB.create_all()
+        build_template_db()
 
 if __name__=="__main__":
     build_db()
