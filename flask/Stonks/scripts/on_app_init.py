a2ce9cb543b5bba55715e84326d727c3dc22a275
distortedlogic <jermeek@users.noreply.github.com>
2019-12-19 12:05:44 -0500

    Update on_app_init.py

--- flask/Stonks/scripts/on_app_init.py
+++ flask/Stonks/scripts/on_app_init.py
@@ -1,6 +1,8 @@
 from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+from flask import current_app as app
+
 from templates import build_template_db
 from schema import DB
 
@@ -10,4 +12,5 @@ def build_db():
     build_template_db()
 
 if __name__=="__main__":
-    build_db()
+    with app.app_context():
+        build_db()
