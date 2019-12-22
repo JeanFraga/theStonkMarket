e8f61fa93824800232ff97bca04512151adacc91
distortedlogic <jermeek@users.noreply.github.com>
2019-12-22 12:32:16 -0500

    dep update

--- flask/Stonks/manage.py
+++ flask/Stonks/manage.py
@@ -1,25 +1,25 @@
-from flask_script import Manager, Server
+# from flask_script import Manager, Server
 
-from Stonks.scripts.templatedb_builder import build_template_db
-from Stonks.schema import DB
-from Stonks.app import create_app
+# from Stonks.scripts.templatedb_builder import build_template_db
+# from Stonks.schema import DB
+# from Stonks.app import create_app
 
-class MyFlaskApp(Flask):
-  def run(self, **kwargs):
-    with self.app_context():
-        first_build_init()
-    super(MyFlaskApp, self).run(**kwargs)
+# class MyFlaskApp(Flask):
+#   def run(self, **kwargs):
+#     with self.app_context():
+#         first_build_init()
+#     super(MyFlaskApp, self).run(**kwargs)
 
-def on_app_init():
-    DB.create_all()
-    build_template_db()
+# def on_app_init():
+#     DB.create_all()
+#     build_template_db()
 
-class CustomServer(Server):
-    def __call__(self, app, *args, **kwargs):
-        on_app_init()
-        return Server.__call__(self, app, *args, **kwargs)
+# class CustomServer(Server):
+#     def __call__(self, app, *args, **kwargs):
+#         on_app_init()
+#         return Server.__call__(self, app, *args, **kwargs)
 
-def create_manager():
-    manager = Manager(app=create_app())
-    manager.add_command('runserver', CustomServer())
-    return manager
+# def create_manager():
+#     manager = Manager(app=create_app())
+#     manager.add_command('runserver', CustomServer())
+#     return manager
